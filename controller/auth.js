cdf0522a95b4cdd2d1b682418bc104147b8ca4e7
Sammo <116154150+sammo-2000@users.noreply.github.com>
2023-10-24 15:53:02 +0100

    Added reset password API

--- controller/auth.js
+++ controller/auth.js
@@ -2,10 +2,14 @@ import bcrypt from 'bcrypt'
 import util from 'util';
 import login_schema from '../schema/login_schema.js'
 import signup_schema from '../schema/signup_schema.js'
+import password_schema from '../schema/password_schema.js'
 import User_Model from '../models/user.js'
+import Password_Model from '../models/Password.js'
 import _email from '../email/send_mail.js'
+import crypto from 'crypto'
 
 const User = new User_Model()
+const Password = new Password_Model()
 
 const login_post = async (req, res) => {
     const { email, password } = req.body
@@ -29,13 +33,6 @@ const login_post = async (req, res) => {
                     req.session.first_name = User.first_name;
                     req.session.last_name = User.last_name;
                     req.session.role = User.role;
-                    console.log('send email')
-                    await _email.main(email, "Welcome to " + process.env.APP_NAME, 'welcome', {
-                        first_name: User.first_name,
-                        last_name: User.last_name,
-                        access_code: User.access_code
-                    })
-                    console.log('email sent')
                     return res.status(200).json({ type: 'success', message: 'User logged in successfully' });
                 } else {
                     return res.status(500).json({ type: 'error', message: 'Internal server error' });
@@ -79,12 +76,117 @@ const signup_post = async (req, res) => {
 
         await User.signup(first_name, last_name, email, hashed_password, access_code)
 
+        await User.get_user_by_email(email)
+
+        await _email.main(email, "Welcome to " + process.env.APP_NAME, 'welcome', {
+            first_name: User.first_name,
+            last_name: User.last_name,
+            access_code: User.access_code
+        })
+
         login_post(req, res)
     } catch (error) {
         console.error('Controller auth:', error);
         return res.status(500).json({ type: 'error', message: 'Internal server error' })
     }
 
+}
+
+const password_reset_get_email = async (req, res) => {
+    const { email } = req.body
+
+    // Handle errors
+    const { error, value } = password_schema(req.body)
+    if (error) {
+        return res.status(400).json({ type: 'error', message: error.details[0].message })
+    }
+
+    try {
+        const data = await User.get_user_by_email(email)
+        if (data) {
+            // Delete old password reset data
+            await Password.delete_old_data()
+            // Check if email already sent
+            const password_data = await Password.get_data_by_email(email)
+            if (password_data) {
+                return res.status(400).json({ type: 'error', message: 'Email already sent' })
+            } else {
+                // Create unique ID for password reset
+                const reset_link = process.env.HTTP + process.env.DOMAIN + ':' + process.env.PORT + '/reset-password/' + crypto.randomBytes(32).toString('hex')
+                // Save it in database
+                await Password.insert_data(email, reset_link)
+                // Send email with link to reset password
+                await _email.main(email, "Reset your password", 'reset_password', {
+                    first_name: data.first_name,
+                    last_name: data.last_name,
+                    reset_link: reset_link
+                })
+                return res.status(200).json({ type: 'success', message: 'Email sent' })
+            }
+        } else {
+            return res.status(400).json({ type: 'error', message: 'Email not found' })
+        }
+    } catch (error) {
+        console.error('Controller auth:', error)
+        return res.status(500).json({ type: 'error', message: 'Internal server error' })
+    }
+}
+
+const load_reset_password_page = async (req, res) => {
+    const reset_link = process.env.HTTP + process.env.DOMAIN + ':' + process.env.PORT + '/reset-password/' + req.params.id
+    const data = await Password.get_data_by_link(reset_link)
+    if (data) {
+        res.render('auth/reset-password-link', { title: 'Reset Password', session: req.session, id: req.params.id })
+    } else {
+        res.status(404).render('404', { title: 'Page not found', session: req.session })
+    }
+}
+
+const password_reset = async (req, res) => {
+    try {
+        // Get data from request
+        const { email, password } = req.body
+
+        // Handle errors
+        const { error, value } = password_schema(req.body)
+        if (error) {
+            return res.status(400).json({ type: 'error', message: error.details[0].message })
+        }
+
+        // Check if password is empty
+        if (typeof password === 'undefined') {
+            return res.status(400).json({ type: 'error', message: 'Password cannot be empty' })
+        }
+
+        const reset_link = process.env.HTTP + process.env.DOMAIN + ':' + process.env.PORT + '/reset-password/' + req.params.id
+        const data = await Password.get_data_by_link(reset_link)
+
+        // Check if link exists
+        if (!data) {
+            return res.status(400).json({ type: 'error', message: 'Link expired' })
+        }
+
+        // Check if email is correct
+        if (data.email !== email) {
+            return res.status(400).json({ type: 'error', message: 'Email incorrect' })
+        }
+
+        // Hash password
+        const hashAsync = util.promisify(bcrypt.hash);
+        const hashed_password = await hashAsync(password, 10)
+
+        // Update password
+        await User.update_password(email, hashed_password)
+
+        // Delete link
+        await Password.delete_link(data.id)
+
+        // Send JSON
+        return res.status(200).json({ type: 'success', message: 'Password updated' })
+    } catch (error) {
+        console.error('Controller auth - password_reset:', error)
+        return res.status(500).json({ type: 'error', message: 'Internal server error' })
+    }
 }
 
 const get_access_code = async () => {
@@ -97,4 +199,4 @@ const get_access_code = async () => {
     }
 }
 
-export default { login_post, signup_post }
+export default { login_post, signup_post, password_reset_get_email, load_reset_password_page, password_reset }
--- routes/auth.js
+++ routes/auth.js
@@ -6,9 +6,13 @@ import middleware from '../middleware/auth.js'
 // Pages
 router.get('/login', middleware.guest, (req, res) => { res.render('auth/login', { title: 'Login', session: req.session }) })
 router.get('/signup', middleware.guest, (req, res) => { res.render('auth/signup', { title: 'Signup', session: req.session }) })
+router.get('/reset-password', middleware.guest, (req, res) => { res.render('auth/reset-password', { title: 'Reset Password', session: req.session }) })
+router.get('/reset-password/:id', middleware.guest, auth.load_reset_password_page)
 
 // API
 router.post('/api/login', middleware.guest, auth.login_post)
 router.post('/api/signup', middleware.guest, auth.signup_post)
+router.post('/api/reset-password', middleware.guest, auth.password_reset_get_email)
+router.post('/api/reset-password/:id', middleware.guest, auth.password_reset)
 
 export default router
