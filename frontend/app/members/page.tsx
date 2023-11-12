2f3873e818c80fdd976d5554d4062e9f05f04d5a
nayan-builds <c2020083@hallam.shu.ac.uk>
2023-11-12 12:42:26 +0000

    Added some pages and components (starting points)

---
+++ frontend/app/members/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import MemberList from "./MemberList";
+
+export default function Members() {
+  return (
+    <>
+      <div>Members</div>
+      <MemberList />
+    </>
+  );
+}
