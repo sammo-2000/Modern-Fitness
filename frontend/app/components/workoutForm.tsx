97b1fbe5edfd487319fafe9e50905c843ea3edd8
hassan755-bit <hassanshab98@icloud.com>
2023-11-15 11:35:41 +0000

    dontworryitsnothing

--- frontend/app/components/workoutForm.tsx
+++ frontend/app/components/workoutForm.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { FormEvent, useState} from "react";
-import { BrowserRouter} from 'react-router-dom'
+// import { BrowserRouter} from 'react-router-dom'
 
 
  export const WorkoutForm = () =>{
