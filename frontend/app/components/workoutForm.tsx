45ded1ccfb2c7450e62a5e2b2890e9a0032a2a2a
hassan755-bit <hassanshab98@icloud.com>
2023-11-18 13:13:23 +0000

    added tailored workout and displauyed them

--- frontend/app/components/workoutForm.tsx
+++ frontend/app/components/workoutForm.tsx
@@ -1,28 +1,50 @@
 "use client";
 import { FormEvent, useState } from "react";
-//
+import { Datepicker } from 'flowbite-react';
 
-export const WorkoutForm = () => {
-  const [Type, SetType] = useState("");
+export const WorkoutForm = ({user_id}: {user_id:any}) => {
+  const [Name, SetName] = useState("");
   const [Load, SetLoad] = useState("");
   const [Reps, SetReps] = useState("");
-  const [Date, SetDate] = useState("");
+  const [DateTime, SetDate] = useState(new Date());
   const [error, setError] = useState(null);
 
-  async function SubmitWorkoutForm(event: FormEvent<HTMLFormElement>) {
+  const SubmitWorkoutForm = async (event: FormEvent<HTMLFormElement>)=>   {
+    console.log("saving form")
     event.preventDefault();
-    const WorkoutJSON = { Type, Load, Reps, Date };
-    console.log(JSON.stringify(WorkoutJSON));
+    // const WorkoutJSON = { Type, Load, Reps, Date };
+    
+    
+    const WorkoutJSON = 
+    { 
+      user_id: user_id, 
+      workout: [{name:Name, load:Load, reps:Reps }],
+      date: DateTime.toISOString().split('T')[0]
 
-    const APIresponse = await fetch("api/workouts", {
+    }
+    console.log(JSON.stringify(WorkoutJSON));
+    const APIresponse = await fetch("http://localhost:3001/api/program", {
       method: "POST",
       body: JSON.stringify(WorkoutJSON),
       headers: {
         "Content-Type": "application/json",
       },
     });
-    const responseJSON = APIresponse.json();
+    const responseJSON = await APIresponse.json();
+    if (!responseJSON.success) {
+      setError(responseJSON.message)
+    }
+    else{
+      setError(null)
+      SetName("")
+      SetLoad("")
+      SetReps("")
+      SetDate(new Date())
+      console.log("Workout has been added: ", responseJSON.program)
+    }
+
   }
+
 
   return (
     // https://v1.tailwindcss.com/components/forms
@@ -49,8 +71,8 @@ export const WorkoutForm = () => {
           <input
             className="focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none sm:w-1/2 md:w-1/3"
             type="text"
-            onChange={(e) => SetType(e.target.value)}
-            value={Type}
+            onChange={(e) => SetName(e.target.value)}
+            value={Name}
             placeholder="Enter Workout Name"
           ></input>
         </div>
@@ -78,25 +100,34 @@ export const WorkoutForm = () => {
             placeholder="Enter the number of reps "
           ></input>
         </div>
+        <div className="relative max-w-sm">
+  <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+     <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+        <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+      </svg>
+  </div>
+  < Datepicker onSelectedDateChanged={(date) =>  SetDate(date)} autoHide= {true} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select End date">
+    </Datepicker>
+</div>
         <div className="mb-6 flex items-center">
           <label className="mb-2 block w-1/2 text-sm font-bold text-gray-700">
             Goals
           </label>
           <textarea
             className="focus:shadow-outline mb-3 w-1/2 rounded border px-3 py-2 leading-tight text-gray-900 focus:outline-none"
-            rows={4}
-            placeholder="Your goals..."
+            rows={6}
+           
           />
         </div>
         <div className="flex items-center justify-between">
           {/* Button css from: https://flowbite.com/docs/components/buttons/ */}
           <button
             className="mb-2 me-2 rounded-lg bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gradient-to-br focus:outline-none focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-800"
-            type="button"
+            type="submit"
           >
             Add
           </button>
-          {error && <div className="error">{error}</div>}
+          {error && <div className="dark:text-red-400">{error}</div>}
         </div>
       </form>
     </div>
--- frontend/app/workout/page.tsx
+++ frontend/app/workout/page.tsx
@@ -1,17 +1,61 @@
-
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
 import WorkoutForm from '../components/workoutForm';
 import WorkoutStats from '../components/workoutStats';
-export default function Workout() {
+interface Workout {
+  name: string;
+  load: number;
+  reps: number;
+  id: number;
+}
+interface Program {
+  workout: Workout[];
+  date:Date;
+ 
+}
+interface ProgramAPIResponse {
+  programs: Program[];
+ 
+}
+
+interface WorkoutStatsProps {
+  workout: Workout;
+}
+
+export const WorkoutPage = ({user_id}: {user_id:any}) => {
+  // this needs to be removed once pages are integrated
+  user_id = "654d1b038077914a27529f0a"
+  const [ProgramResponse, SetProgramResponse ]  = useState<ProgramAPIResponse |null >(null)
+  useEffect(() => {
+    const fetchPrograms = async () => {
+      const response = await fetch('http://localhost:3001/api/programs/' + user_id)
+
+      const json = await response.json()
+
+      if (response.ok) {
+        console.log(json)
+        SetProgramResponse(json)
+      }
+    }
+
+    fetchPrograms()
+  }, [])
+
   return (
     <div className= "  bg-gray-300 min-h-screen">
-       <WorkoutForm />
+       <WorkoutForm user_id={user_id} />
        <div className= "min-h-screen bg-gray-300">
       
-       
+       {ProgramResponse && ProgramResponse.programs.map(program => (
+  program.workout && program.workout.map(workout => (
+    <WorkoutStats key={workout.id} date = {program.date} workout={workout} />
+  ))
+))}
+        
       </div>
      
     </div>
   )
 }
 // workout={workout}/>
+export default WorkoutPage
